8b6b49b5835fefd0af59e39f41270b2da9b66c17
Waleed Ahmed <w.ahmed1801@gmail.com>
2024-06-05 13:12:16 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var a= ('<b>Rules for namimg JS variable</b> </br> ') 
+ document.write(a)
+
+ var b=('Variable name can only contains,numbers, <b>$</b> and________.<b>F</b>or ,Example: <b>$ </b>my first vaiable </br>')
+ document.write(b)
+
+ var c=('Variable Must begin with a letter, <b>$</b> Or________.<b>F</b>or ,Example: <b>$ </b>Name _______ name or name </br> ')
+ document.write(c)
+ var d=('Variable Names are case sensitive </br> ')
+ document.write(d)
+ var d=('Variable Names Should not be JS keywords')
+ document.write(d)
